7136cbc38c55934c47bd663b4e9d7ff757d87aa4
Toni Fisler <mail@toni.io>
2020-02-27 11:12:50 +0100

    prettier the things

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from "react"
 
-const abilities = ['FOR', 'DEX', 'CON', 'INT', 'SAG', 'CHA'];
+const abilities = ["FOR", "DEX", "CON", "INT", "SAG", "CHA"]
 
-export const MarkLink = ({text, link, style}) => (
+export const MarkLink = ({ text, link, style }) => (
   <a
     href={link}
     target="_blank"
@@ -12,25 +12,27 @@ export const MarkLink = ({text, link, style}) => (
   >
     <mark>{text}</mark>
   </a>
-);
+)
 
-export const Test = ({ability, mods}) => {
-
+export const Test = ({ ability, mods }) => {
   if (abilities.indexOf(ability) === -1) {
-    return (<span className="text-danger">"{ability}" is not a valid ability</span>)
+    return (
+      <span className="text-danger">"{ability}" is not a valid ability</span>
+    )
   }
 
   return (
     <em className="text-check">
-      Test de {ability}{mods}
+      Test de {ability}
+      {mods}
     </em>
   )
 }
 
 export const Advantage = () => (
-  <span title="Avantage" className="badge-advantage"/>
-);
+  <span title="Avantage" className="badge-advantage" />
+)
 
 export const Disadvantage = () => (
-  <span title="Désavantage" className="badge-disadvantage"/>
-);
+  <span title="Désavantage" className="badge-disadvantage" />
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,10 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <p>Custom DND helpers from <a href="https://twitter.com/tonifisler">@tonifisler</a></p>
+    <p>
+      Custom DND helpers from{" "}
+      <a href="https://twitter.com/tonifisler">@tonifisler</a>
+    </p>
     <ul>
       <li>
         <Link to="/player-cheat-sheet/">Player Cheat Sheet</Link>
